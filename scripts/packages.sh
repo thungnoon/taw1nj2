1afceb55e7c798a361f3f8f9f6a1b16d2bec5f0d
thungnoon <kongzheng.zhou@gmail.com>
2024-08-31 17:13:24 +0800

    Update packages.sh

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -33,6 +33,8 @@ git clone -b main --depth 1 https://github.com/xiaorouji/openwrt-passwall2.git
 git clone -b v5 --depth 1 https://github.com/sbwml/luci-app-mosdns.git
 # #openclash
 git clone -b master --depth 1 https://github.com/vernesong/OpenClash.git
+# #poweroffdevice
+git clone -b main --depth 1 https://github.com/sirpdboy/luci-app-poweroffdevice.git
 
 popd
 
