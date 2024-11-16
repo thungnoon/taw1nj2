62bff3156b8d2f8dfc2edd735227d707f66a087e
thungnoon <kongzheng.zhou@gmail.com>
2024-11-16 17:08:11 +0800

    Update packages.sh

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -6,15 +6,12 @@
 rm -rf ./feeds/packages/net/shadowsocks-libev
 rm -rf ./feeds/packages/net/chinadns-ng
 
-
 #克隆依赖插件
 git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/pwpage
-
 
 #克隆的源码放在small文件夹
 mkdir package/small
 pushd package/small
-
 
 #adguardhome
 git clone -b 2023.10 --depth 1 https://github.com/XiaoBinin/luci-app-adguardhome.git
@@ -43,3 +40,4 @@ git clone -b main --depth 1 https://github.com/morytyann/OpenWrt-mihomo.git
 
 popd
 
+echo "packages executed successfully!"
