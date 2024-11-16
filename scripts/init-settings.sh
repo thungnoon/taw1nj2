92a8323842d81cba07dee73754c92f6d6064aa36
thungnoon <kongzheng.zhou@gmail.com>
2024-11-16 17:08:57 +0800

    Update init-settings.sh

--- scripts/init-settings.sh
+++ scripts/init-settings.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-#更改默认地址为192.168.8.1
-sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
-
-
+#更改默认地址为192.168.5.1
+sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
 
 echo "init-settings executed successfully!"
