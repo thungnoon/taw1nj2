3faab8caead6cc787fcc916518dd0268657bc802
thungnoon <kongzheng.zhou@gmail.com>
2025-02-21 08:59:10 +0800

    Update init-settings.sh

--- scripts/init-settings.sh
+++ scripts/init-settings.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-sed -i 's/KERNEL_PATCHVER:=6.6/KERNEL_PATCHVER:=6.12/g' ./target/linux/x86/Makefile
 
 #更改默认地址为192.168.5.1
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
